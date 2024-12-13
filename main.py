511888f64f8b85c23b480f14e01a206f6c7e5f93
Arslene-BELHADJ <arslene.belhadj@edu.esiee.fr>
2024-12-13 14:37:16 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-#### Imports et définition des variables globales
-
+"""Imports et définition des variables globales"""
+import sys
+sys.setrecursionlimit(1100)
 
 #### Fonctions secondaires
 
 
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
+    """retourne la liste de tuples encodant une chaîne de caractères passée en 
+    argument selon un algorithme itératif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -13,14 +15,26 @@ def artcode_i(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
 
-    return [ ]
+    # votre code ici
+    tableau = []
+    compteur = 1
+    if len(s)==0:
+        return tableau
+    for i in range (1, len(s)) :
+        if s[i-1]==s[i]:
+            compteur +=1
+        else :
+            tableau.append((s[i-1],compteur))
+            compteur = 1
+    tableau.append((s[i],compteur))
+    return tableau
+
 
 
 def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
+    """retourne la liste de tuples encodant une chaîne de caractères passée en argument 
+    selon un algorithme récursif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -28,20 +42,27 @@ def artcode_r(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
+
     # votre code ici
 
     # cas de base
     # recherche nombre de caractères identiques au premier
     # appel récursif
+    tableau = []
+    compteur = 1
+    if len(s)==0:
+        return tableau
+    while compteur < len(s) and s[compteur] == s[0]:
+        compteur += 1
+    tableau.append((s[0],compteur))
+    return tableau + artcode_r(s[compteur:])
 
-    return []
-    
 
-#### Fonction principale
+#### Fonction principale###
 
 
 def main():
+    """Fonction main permettant le bon fonctionnement du code"""
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
 
